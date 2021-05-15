0a0d70f85f50a5d59a1b8747a5c300244d7222b8
Ngọc Lam <nglamvn2911@gmail.com>
2021-05-15 15:00:55 +0700

    Bruh lol lolol

--- plugins/NgTest/src/NgLamVN/NgTest/Main.php
+++ plugins/NgTest/src/NgLamVN/NgTest/Main.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace NgLamVN\NgTest;
 
+use jojoe77777\FormAPI\CustomForm;
+use pocketmine\block\Block;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\Listener;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase implements Listener
@@ -25,6 +28,18 @@ class Main extends PluginBase implements Listener
 		$block = $event->getBlock();
 		$player = $event->getPlayer();
 
-		$player->sendMessage("You have break block: " . $block->getName());
+		$this->BreakForm($player, $block);
+	}
+
+	public function BreakForm (Player $player, Block $block)
+	{
+		$form = new CustomForm(function(Player $player, array $data)
+		{
+		});
+
+		$form->setTitle("Block Break Info");
+		$form->addLabel("You have break block: " . $block->getName());
+
+		$player->sendForm($form);
 	}
 }
