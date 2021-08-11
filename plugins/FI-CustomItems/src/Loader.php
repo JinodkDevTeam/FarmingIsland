277e3f30540a75004087c1c5e92f97aa86aaa0e4
Ngọc Lam <nglamvn2911@gmail.com>
2021-08-11 21:17:46 +0700

    CustomItems: Make it work (WIP)

--- plugins/FI-CustomItems/src/Loader.php
+++ plugins/FI-CustomItems/src/Loader.php
@@ -3,7 +3,12 @@ declare(strict_types=1);
 
 namespace CustomItems;
 
+use CustomItems\item\CustomItemFactory;
 use pocketmine\plugin\PluginBase;
 
 class Loader extends PluginBase{
+
+	public function onEnable() : void{
+		new CustomItemFactory();
+	}
 }
