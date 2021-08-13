9b9d53424bb2e5e68c3f8b992d26e32bc53f80ae
Ngọc Lam <nglamvn2911@gmail.com>
2021-08-13 15:45:44 +0700

    CustomItem: Add event handler

--- plugins/FI-CustomItems/src/Loader.php
+++ plugins/FI-CustomItems/src/Loader.php
@@ -5,12 +5,14 @@ namespace CustomItems;
 
 use CustomItems\enchantment\CustomEnchantGlint;
 use CustomItems\item\CustomItemFactory;
+use CustomItems\listener\CustomItemListener;
 use pocketmine\data\bedrock\EnchantmentIdMap;
 use pocketmine\plugin\PluginBase;
 
 class Loader extends PluginBase{
 
 	public function onEnable() : void{
+		$this->getServer()->getPluginManager()->registerEvents(new CustomItemListener(), $this);
 		new CustomItemFactory();
 		EnchantmentIdMap::getInstance()->register(100, new CustomEnchantGlint());
 	}
---
+++ plugins/FI-CustomItems/src/listener/CustomItemListener.php
@@ -0,0 +1,78 @@
+<?php
+declare(strict_types=1);
+
+namespace CustomItems\listener;
+
+use CustomItems\item\CustomItemFactory;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerItemUseEvent;
+
+class CustomItemListener implements Listener{
+	/**
+	 * @param BlockPlaceEvent $event
+	 * @priority HIGH
+	 * @handleCancelled FALSE
+	 */
+	public function onPlace(BlockPlaceEvent $event): void{
+		$item = $event->getItem();
+		if ($item->getNamedTag()->getTag("CustomItemID") !== null){
+			$event->cancel();
+		}
+	}
+
+	/**
+	 * @param PlayerInteractEvent $event
+	 * @priority MONITOR
+	 * @handleCancelled FALSE
+	 */
+	public function onInteract(PlayerInteractEvent $event){
+		$item = $event->getItem();
+		if ($item->getNamedTag()->getTag("CustomItemID") !== null){
+			$citem = CustomItemFactory::getInstance()->get((int)$item->getNamedTag()->getTag("CustomItemID"));
+			if ($citem == null) return;
+
+			$player = $event->getPlayer();
+			$blockClicked = $event->getBlock();
+			$face = $event->getFace();
+			$clickVector = $event->getTouchVector();
+			$blockReplace = $blockClicked->getSide($face);
+			$citem->onInteractBlock($player, $blockReplace, $blockClicked, $face, $clickVector);
+		}
+	}
+
+	/**
+	 * @param PlayerItemUseEvent $event
+	 * @priority MONITOR
+	 * @handleCancelled FALSE
+	 */
+	public function onItemUse(PlayerItemUseEvent $event){
+		$item = $event->getItem();
+		if ($item->getNamedTag()->getTag("CustomItemID") !== null){
+			$citem = CustomItemFactory::getInstance()->get((int)$item->getNamedTag()->getTag("CustomItemID"));
+			if ($citem == null) return;
+
+			$player = $event->getPlayer();
+			$directionVector = $event->getDirectionVector();
+			$citem->onClickAir($player, $directionVector);
+		}
+	}
+
+	/**
+	 * @param BlockBreakEvent $event
+	 * @priority MONITOR
+	 * @handleCancelled FALSE
+	 */
+	public function onBreak(BlockBreakEvent $event){
+		$item = $event->getItem();
+		if ($item->getNamedTag()->getTag("CustomItemID") !== null){
+			$citem = CustomItemFactory::getInstance()->get((int)$item->getNamedTag()->getTag("CustomItemID"));
+			if ($citem == null) return;
+
+			$block = $event->getBlock();
+			$citem->onDestroyBlock($block);
+		}
+	}
+}
