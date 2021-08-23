3d62a80cb9e6f8b08d492e00d59f137743525398
NgLamIsSimp <nglamvn2911@gmail.com>
2021-08-23 14:09:29 +0700

    Nothing.

---
+++ plugins/RandomKnockback/src/RandomKnockback.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace RandKB;
+
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+class RandomKnockback extends PluginBase implements Listener{
+
+	public function onEnable() : void{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	/**
+	 * @param EntityDamageByEntityEvent $event
+	 * @priority HIGHEST
+	 * @handleCancelled FALSE
+	 */
+	public function onCombat(EntityDamageByEntityEvent $event){
+		$knockback = mt_rand(0, 100);
+		$event->setKnockBack($knockback / 10);
+	}
+}
