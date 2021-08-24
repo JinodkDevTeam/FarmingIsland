82b3085696d50d7ecf43f61bbfe8b818a6ec5c1d
NgLamIsSimp <nglamvn2911@gmail.com>
2021-08-24 10:34:33 +0700

    RankPrefix: Cleanup

--- plugins/FI-RankPrefix/src/Loader.php
+++ plugins/FI-RankPrefix/src/Loader.php
@@ -23,52 +23,26 @@ class Loader extends PluginBase implements Listener
 	public function getPrefix(Player $player): string
 	{
 		if (!isset($this->prefix[$player->getName()])) return UnicodeParser::fromCode("E100");
-		switch($this->prefix[$player->getName()])
-		{
-			case "vip":
-				return UnicodeParser::fromCode("E101");
-			case "admin":
-				return UnicodeParser::fromCode("E102");
-			case "staff":
-				return UnicodeParser::fromCode("E103");
-			case "member":
-				return ""; //U+E104
-			case "youtuber":
-			case "ytber":
-				return ""; //U+E105
-			case "developer":
-			case "dev":
-				return ""; //U+E106
-			case "owner":
-				return ""; //U+E107
-			case "helper":
-				return ""; //U+E108
-			case "catlazy":
-				return ""; //U+E110
-			case "rgb":
-				return ""; //U+E111
-			case "nglam":
-				return ""; //U+E112
-			case "jinodk":
-				return ""; //U+E113
-			case "uwu":
-				return ""; //U+E114
-			case "wibu":
-			case "weeb":
-			case "weeeb":
-				return ""; //U+E115
-			case "simp":
-				return ""; //U+E116
-			case "lgbt":
-			case "lgbtq+":
-				return ""; //U+E117
-			case "campiole":
-			case "camp":
-				return ""; //U+E118
-			case "player":
-			default:
-				return UnicodeParser::fromCode("E100");
-		}
+		return match ($this->prefix[$player->getName()]) {
+			"vip" => UnicodeParser::fromCode("E101"),
+			"admin" => UnicodeParser::fromCode("E102"),
+			"staff" => UnicodeParser::fromCode("E103"),
+			"member" => UnicodeParser::fromCharactor("E104"),
+			"youtuber", "ytber" => UnicodeParser::fromCharactor("E105"),
+			"developer", "dev" => UnicodeParser::fromCharactor("E106"),
+			"owner" => UnicodeParser::fromCharactor("E107"),
+			"helper" => UnicodeParser::fromCharactor("E108"),
+			"catlazy" => UnicodeParser::fromCharactor("E110"),
+			"rgb" => UnicodeParser::fromCharactor("E111"),
+			"nglam" => UnicodeParser::fromCharactor("E112"),
+			"jinodk" => UnicodeParser::fromCharactor("E113"),
+			"uwu" => UnicodeParser::fromCharactor("E114"),
+			"wibu", "weeb", "weeeb" => UnicodeParser::fromCharactor("E115"),
+			"simp" => UnicodeParser::fromCharactor("E116"),
+			"lgbt", "lgbtq+" => UnicodeParser::fromCharactor("E117"),
+			"campiole", "camp" => UnicodeParser::fromCharactor("E118"),
+			default => UnicodeParser::fromCharactor("E100"),
+		};
 	}
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool
