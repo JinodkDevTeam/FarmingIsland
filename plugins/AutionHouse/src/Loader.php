412bf55531317cc781cdc1ed90bf3e0b2752d096
NgLamIsSimp <nglamvn2911@gmail.com>
2021-08-24 10:13:12 +0700

    ok.

--- plugins/AutionHouse/src/Loader.php
+++ plugins/AutionHouse/src/Loader.php
@@ -3,8 +3,23 @@ declare(strict_types=1);
 
 namespace AutionHouse;
 
+use AutionHouse\provider\SqliteProvider;
 use pocketmine\plugin\PluginBase;
 
 class Loader extends PluginBase{
 
+	protected SqliteProvider $provider;
+
+	public function onEnable(): void{
+		$this->provider = new SqliteProvider($this);
+		$this->getProvider()->init();
+	}
+
+	public function onDisable(): void{
+		$this->getProvider()->close();
+	}
+
+	public function getProvider(): SqliteProvider{
+		return $this->provider;
+	}
 }
