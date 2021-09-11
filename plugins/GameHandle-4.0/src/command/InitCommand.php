0c209d4ac47ca9b1f06da74f502f584a46107ab1
NgLamIsSimp <nglamvn2911@gmail.com>
2021-09-11 17:25:07 +0700

    GameHandle: reformat init command class.

--- plugins/GameHandle-4.0/src/command/InitCommand.php
+++ plugins/GameHandle-4.0/src/command/InitCommand.php
@@ -8,32 +8,32 @@ use NgLamVN\GameHandle\Core;
 
 class InitCommand
 {
-    public function __construct(Core $plugin)
-    {
-        $cmd = $plugin->getServer()->getCommandMap();
+	public function __construct(Core $plugin)
+	{
+		$cmd = $plugin->getServer()->getCommandMap();
 
-        $cmd->register("heal", new Heal($plugin));
-        $cmd->register("feed", new Feed($plugin));
-        $cmd->register("fly", new Fly($plugin));
-        $cmd->register("gm1", new Gm1($plugin));
-        $cmd->register("gm0", new Gm0($plugin));
-        $cmd->register("gm2", new Gm2($plugin));
-        $cmd->register("gm3", new Gm3($plugin));
-        $cmd->register("sell", new Sell($plugin));
-        $cmd->register("tpall", new TpAll($plugin));
-        $cmd->register("fiversion", new FiVersion($plugin));
-        $cmd->register("sudo", new Sudo($plugin));
-        $cmd->register("smartmine", new SmartMine($plugin));
-        $cmd->register("dupe", new Dupe($plugin));
-        $cmd->register("tutorial", new Tutorial($plugin));
-        $cmd->register("servercheck", new ServerCheck($plugin));
-        $cmd->register("mute", new Mute($plugin));
-        $cmd->register("unmute", new UnMute($plugin));
-        $cmd->register("freeze", new Freeze($plugin));
-        $cmd->register("unfreeze", new UnFreeze($plugin));
-        $cmd->register("notp", new NoTP($plugin));
-        $cmd->register("icgive", new IcGive($plugin)); //TODO: Slove NBT problem
+		$cmd->register("heal", new Heal($plugin));
+		$cmd->register("feed", new Feed($plugin));
+		$cmd->register("fly", new Fly($plugin));
+		$cmd->register("gm1", new Gm1($plugin));
+		$cmd->register("gm0", new Gm0($plugin));
+		$cmd->register("gm2", new Gm2($plugin));
+		$cmd->register("gm3", new Gm3($plugin));
+		$cmd->register("sell", new Sell($plugin));
+		$cmd->register("tpall", new TpAll($plugin));
+		$cmd->register("fiversion", new FiVersion($plugin));
+		$cmd->register("sudo", new Sudo($plugin));
+		$cmd->register("smartmine", new SmartMine($plugin));
+		$cmd->register("dupe", new Dupe($plugin));
+		$cmd->register("tutorial", new Tutorial($plugin));
+		$cmd->register("servercheck", new ServerCheck($plugin));
+		$cmd->register("mute", new Mute($plugin));
+		$cmd->register("unmute", new UnMute($plugin));
+		$cmd->register("freeze", new Freeze($plugin));
+		$cmd->register("unfreeze", new UnFreeze($plugin));
+		$cmd->register("notp", new NoTP($plugin));
+		$cmd->register("icgive", new IcGive($plugin)); //TODO: Slove NBT problem
 		$cmd->register("cgive", new CGive($plugin));
 		$cmd->register("pos", new Pos($plugin));
-    }
+	}
 }
