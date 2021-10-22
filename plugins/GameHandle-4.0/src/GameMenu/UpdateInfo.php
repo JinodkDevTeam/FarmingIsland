84f70be208a05297cac5196ad7d2b872cf329207
NgLamIsSimp <nglamvn2911@gmail.com>
2021-10-22 21:37:15 +0700

    Lazy.

--- plugins/GameHandle-4.0/src/GameMenu/UpdateInfo.php
+++ plugins/GameHandle-4.0/src/GameMenu/UpdateInfo.php
@@ -22,7 +22,7 @@ class UpdateInfo{
 		$text = [
 			"§　Updates:",
 			"- Welcome to API 4.0.0 branch for FarmingIslandServer",
-			"Thông báo: GUI không còn được update trên Island Menu, các bạn vui lòng chuyển sang sử dụng UI trên Island Menu",
+			"- View changelogs at changelogs",
 			"Official wiki: bit.ly/fi-wiki",
 			"Vote for server: bit.ly/fi-vote",
 			"Official Facebook group: bit.ly/jinodkgroupfb",
