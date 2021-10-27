c7e476cffed41e2f476f0b1e43658e2d42156f17
NgLamIsSimp <nglamvn2911@gmail.com>
2021-10-27 10:13:49 +0700

    GrowableSneak: Add missing player.

--- plugins/FI-GrowableSneak/src/Loader.php
+++ plugins/FI-GrowableSneak/src/Loader.php
@@ -9,6 +9,7 @@ use pocketmine\block\utils\TreeType;
 use pocketmine\event\block\StructureGrowEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerToggleSneakEvent;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Random;
 use pocketmine\world\generator\object\TreeFactory;
@@ -36,10 +37,10 @@ class Loader extends PluginBase implements Listener{
 		if(!$event->isSneaking()){
 			true;
 		}
-		$this->getArea($event->getPlayer()->getPosition());
+		$this->getArea($event->getPlayer()->getPosition(), $event->getPlayer());
 	}
 
-	public function getArea(Position $position){
+	public function getArea(Position $position, Player $player){
 		$x = $position->getX();
 		$y = $position->getY();
 		$z = $position->getZ();
@@ -49,16 +50,16 @@ class Loader extends PluginBase implements Listener{
 			for($j = $y - $radius; $j <= $y + $radius; $j++)
 				for($k = $z - $radius; $k <= $z + $radius; $k++){
 					$block = $world->getBlockAt((int) $i, (int) $j, (int) $k);
-					$this->growTree($block);
+					$this->growTree($block, $player);
 				}
 	}
 
-	public function growTree(Block $block){
+	public function growTree(Block $block, Player $player){
 		if(!$block instanceof Sapling){
 			return;
 		}
 		if(mt_rand(1, 20) === 5){
-			$this->grow($block);
+			$this->grow($block, $player);
 		}else{
 			$pos = $block->getPosition()->asVector3();
 			$pos->y++;
@@ -66,7 +67,7 @@ class Loader extends PluginBase implements Listener{
 		}
 	}
 
-	public function grow(Sapling $sapling) : void{
+	public function grow(Sapling $sapling, Player $player) : void{
 		$random = new Random(mt_rand());
 		$tree = TreeFactory::get($random, TreeType::fromMagicNumber($sapling->getMeta()));
 		$transaction = $tree?->getBlockTransaction($sapling->getPosition()->getWorld(), $sapling->getPosition()->getFloorX(), $sapling->getPosition()->getFloorY(), $sapling->getPosition()->getFloorZ(), $random);
@@ -74,7 +75,7 @@ class Loader extends PluginBase implements Listener{
 			return;
 		}
 
-		$ev = new StructureGrowEvent($sapling, $transaction);
+		$ev = new StructureGrowEvent($sapling, $transaction, $player);
 		$ev->call();
 		if($ev->isCancelled()){
 			return;
