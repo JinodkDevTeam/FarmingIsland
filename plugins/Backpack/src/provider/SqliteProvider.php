26bb3aba070b2922575f023e4162414b594332c3
NgLamIsSimp <nglamvn2911@gmail.com>
2021-10-29 10:39:47 +0700

    Backback: complete database api.

--- plugins/Backpack/src/provider/SqliteProvider.php
+++ plugins/Backpack/src/provider/SqliteProvider.php
@@ -5,6 +5,7 @@ namespace Backpack\provider;
 
 use Backpack\Loader;
 use Generator;
+use pocketmine\player\Player;
 use poggit\libasynql\DataConnector;
 use poggit\libasynql\libasynql;
 use SOFe\AwaitGenerator\Await;
@@ -32,7 +33,7 @@ class SqliteProvider{
 		return $this->loader;
 	}
 
-	protected function asyncSelect(string $query, array $args) : Generator{
+	protected function asyncSelect(string $query, array $args = []) : Generator{
 		$this->database->executeSelect($query, $args, yield, yield Await::REJECT);
 		return yield Await::ONCE;
 	}
@@ -41,9 +42,63 @@ class SqliteProvider{
 		$this->database = libasynql::create($this->getLoader(), $this->getLoader()->getConfig()->get("database"), [
 			"sqlite" => "sqlite.sql"
 		]);
+
+		$this->database->executeGeneric(self::INIT);
 	}
 
 	public function close() : void{
 		if (isset($this->database)) $this->database->close();
+	}
+
+	public function register(Player|string $player, int $slot = 0, string $data = "") : void{
+		if ($player instanceof Player) $player = $player->getName();
+		$this->database->executeChange(self::REGISTER, [
+			"player" => $player,
+			"slot" => $slot,
+			"data" => $data
+		]);
+	}
+
+	public function removeAll(Player|string $player) : void{
+		if ($player instanceof Player) $player = $player->getName();
+		$this->database->executeChange(self::REMOVE_ALL, [
+			"player" => $player
+		]);
+	}
+
+	public function removeSlot(Player|string $player, int $slot = 0) : void{
+		if ($player instanceof Player) $player = $player->getName();
+		$this->database->executeChange(self::REMOVE_SLOT, [
+			"player" => $player,
+			"slot" => $slot
+		]);
+	}
+
+	public function update(Player|string $player, int $slot = 0, string $data = ""){
+		if ($player instanceof Player) $player = $player->getName();
+		$this->database->executeChange(self::UPDATE, [
+			"player" => $player,
+			"slot" => $slot,
+			"data" => $data
+		]);
+	}
+
+	public function selectAll() : Generator{
+		return $this->asyncSelect(self::SELECT_ALL);
+	}
+
+	public function selectPlayer(Player|string $player) : Generator{
+		if ($player instanceof Player) $player = $player->getName();
+		return $this->asyncSelect(self::SELECT_PLAYER, [
+			"player" => $player
+		]);
+	}
+
+	public function selectSlot(Player|string $player, int $slot) : Generator{
+		if ($player instanceof Player) $player = $player->getName();
+		return $this->asyncSelect(self::SELECT_SLOT, [
+			"player" => $player,
+			"slot" => $slot
+		]);
 	}
 }
