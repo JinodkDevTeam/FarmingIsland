43473c9e16b7aa051119400cd44476307cd1da70
NgLamIsSimp <nglamvn2911@gmail.com>
2021-11-11 17:21:41 +0700

    Fishing: Bug fixes.

--- plugins/FI-FishingModule/src/FishingModule/entity/FishingHook.php
+++ plugins/FI-FishingModule/src/FishingModule/entity/FishingHook.php
@@ -29,13 +29,15 @@ use pocketmine\world\World;
 class FishingHook extends Projectile{
 
 	protected $gravity = 0.1;
-	protected $drag = 0.5;
+	protected $drag = 0.2;
 
-	protected int $ticksCatchable = 0;
-	protected int $ticksCaughtDelay = 0;
-	protected int $ticksCatchableDelay = 0;
+	protected int $ticksCatchable = 0; //How much time player can cactch items after hook is catchable
+	protected int $ticksCaughtDelay = 0; //How much time player need to wait until hook is ready to catch
+	protected int $ticksCatchableDelay = 0; //How much time player need to wait from hook is ready to catch to catchable (Line of particle appeard)
 	protected float $fishApproachAngle = 0;
 	protected Random $random;
+	//CaughtDelay (long) -> CatchableDelay (short) -> (able to catch) -> Catchable -> (unable to catch)
+
 
 	public function __construct(Location $location, ?Human $shootingEntity, ?CompoundTag $nbt = null){
 		$this->random = new Random(intval(microtime(true) * 1000));
@@ -77,7 +79,7 @@ class FishingHook extends Projectile{
 		if ($owner === null){
 			return false;
 		}
-		$inGround = $this->getWorld()->getBlock($this->getPosition()->asVector3())->isSolid() or (!$this->getWorld()->getBlock($this->getPosition()->asVector3()) instanceof Liquid);
+		$inGround = $this->getWorld()->getBlock($this->getPosition()->asVector3())->isSolid();
 
 		if($inGround){
 			$this->motion->x *= $this->random->nextFloat() * 0.2;
@@ -85,13 +87,11 @@ class FishingHook extends Projectile{
 			$this->motion->z *= $this->random->nextFloat() * 0.2;
 		}
 
-		$hasUpdate = parent::onUpdate($currentTick);
-
 		if($owner->isClosed() or !$owner->isAlive() or !($owner->getInventory()->getItemInHand() instanceof FishingRod) or $owner->getPosition()->distanceSquared($this->getPosition()) > 1024){
 			$this->flagForDespawn();
 		}
 
-		if(!$inGround){
+		if((!$inGround) and $this->getWorld()->getBlock($this->getPosition()->asVector3()) instanceof Liquid){ //Check if hook is not in ground.
 			$hasUpdate = true;
 
 			$f6 = 0.92;
@@ -115,95 +115,100 @@ class FishingHook extends Projectile{
 				}
 			}
 
-			if($this->getPosition()->isValid() and $d10 > 0){
-				$l = 1;
-
-				// TODO: lightninstrike
-
-				if($this->ticksCatchable > 0){
-					--$this->ticksCatchable;
-
-					if($this->ticksCatchable <= 0){
-						$this->ticksCaughtDelay = 0;
-						$this->ticksCatchableDelay = 0;
-					}
-				}elseif($this->ticksCatchableDelay > 0){
-					$this->ticksCatchableDelay -= $l;
-
-					if($this->ticksCatchableDelay <= 0){
-						$this->broadcastAnimation(new FishingHookHookAnimation($this));
-
-						$this->motion->y -= 0.02;
-						$this->ticksCatchable = mt_rand(10, 30);
-					}else{
-						$this->fishApproachAngle = $this->fishApproachAngle + $this->random->nextSignedFloat() * 4.0;
-						$f7 = $this->fishApproachAngle * 0.01745;
-						$f10 = sin($f7);
-						$f11 = cos($f7);
-						$d13 = $this->getPosition()->getX() + ($f10 * $this->ticksCatchableDelay * 0.1);
-						$d15 = $this->getPosition()->getY() + 1;
-						$d16 = $this->getPosition()->getZ() + ($f11 * $this->ticksCatchableDelay * 0.1);
-						$block1 = $this->getWorld()->getBlock(new Vector3($d13, $d15 - 1, $d16));
-
-						if($block1 instanceof Water){
-							if($this->random->nextFloat() < 0.15){
-								$this->getWorld()->addParticle(new Vector3($d13, $d15 - 0.1, $d16), new WaterParticle());
-							}
-							$this->getWorld()->addParticle(new Vector3($d13, $d15, $d16), new WaterParticle());
-						}
-					}
-				}elseif($this->ticksCaughtDelay > 0){
-					$this->ticksCaughtDelay -= $l;
-					$f1 = 0.15;
-
-					if($this->ticksCaughtDelay < 20){
-						$f1 = ($f1 + (20 - $this->ticksCaughtDelay) * 0.05);
-					}elseif($this->ticksCaughtDelay < 40){
-						$f1 = ($f1 + (40 - $this->ticksCaughtDelay) * 0.02);
-					}elseif($this->ticksCaughtDelay < 60){
-						$f1 = ($f1 + (60 - $this->ticksCaughtDelay) * 0.01);
-					}
-
-					if($this->random->nextFloat() < $f1){
-						$f9 = mt_rand(0, 360) * 0.01745;
-						$f2 = mt_rand(25, 60);
-						$d12 = $this->getPosition()->getX() + (sin($f9) * $f2 * 0.1);
-						$d14 = floor($this->getPosition()->getY()) + 1.0;
-						$d6 = $this->getPosition()->getZ() + (cos($f9) * $f2 * 0.1);
-						$block = $this->getWorld()->getBlock(new Vector3($d12, $d14 - 1, $d6));
-
-						if($block instanceof Water){
-							$this->getWorld()->addParticle(new Vector3($d12, $d14, $d6), new SplashParticle());
-						}
-					}
-
-					if($this->ticksCaughtDelay <= 0){
-						$this->ticksCatchableDelay = mt_rand(20, 80);
-						$this->fishApproachAngle = mt_rand(0, 360);
-					}
-				}else{
-					$this->ticksCaughtDelay = mt_rand(100, 900);
-					$this->ticksCaughtDelay -= 20 * 5; // TODO: Lure
-				}
-				if($this->ticksCatchable > 0){
-					$this->motion->y -= ($this->random->nextFloat() * $this->random->nextFloat() * $this->random->nextFloat()) * 0.2;
-				}
-			}
-
 			$d11 = $d10 * 2.0 - 1.0;
-			$this->motion->y += 0.04 * $d11;
+			$this->motion->y += 0.04 * $d11; //Rise up.
 
 			if($d10 > 0.0){
 				$f6 = $f6 * 0.9;
 				$this->motion->y *= 0.8;
 			}
 
-			$this->getMotion()->multiply($f6);
+			$this->motion->x *= $f6;
+			$this->motion->y *= $f6;
+			$this->motion->z *= $f6;
+
+			if($this->getPosition()->isValid() and $d10 > 0){
+				$this->handleFishingUpdate();
+			}
 		}
-
-		$this->getOwningEntity()?->teleport($this->getPosition()->add(0, 1, 0));
-
+		if (!isset($hasUpdate)){
+			$hasUpdate = parent::onUpdate($currentTick);
+		}
 		return $hasUpdate;
+	}
+
+	public function handleFishingUpdate(): void{
+		$l = 1;
+		// TODO: lightninstrike
+
+		if($this->ticksCatchable > 0){
+			--$this->ticksCatchable;
+
+			if($this->ticksCatchable <= 0){
+				$this->ticksCaughtDelay = 0;
+				$this->ticksCatchableDelay = 0;
+			}
+		}elseif($this->ticksCatchableDelay > 0){
+			$this->ticksCatchableDelay -= $l;
+
+			if($this->ticksCatchableDelay <= 0){ // Catch
+				$this->broadcastAnimation(new FishingHookHookAnimation($this));
+
+				$this->motion->y -= 0.02;
+				$this->ticksCatchable = mt_rand(10, 30);
+			}else{
+				$this->fishApproachAngle = $this->fishApproachAngle + $this->random->nextSignedFloat() * 4.0;
+				$f7 = $this->fishApproachAngle * 0.01745;
+				$f10 = sin($f7);
+				$f11 = cos($f7);
+				$d13 = $this->getPosition()->getX() + ($f10 * $this->ticksCatchableDelay * 0.1);
+				$d15 = $this->getPosition()->getY() + 1;
+				$d16 = $this->getPosition()->getZ() + ($f11 * $this->ticksCatchableDelay * 0.1);
+				$block1 = $this->getWorld()->getBlock(new Vector3($d13, $d15 - 1, $d16));
+
+				if($block1 instanceof Water){
+					if($this->random->nextFloat() < 0.15){
+						$this->getWorld()->addParticle(new Vector3($d13, $d15 - 0.1, $d16), new WaterParticle());
+					}
+					$this->getWorld()->addParticle(new Vector3($d13, $d15, $d16), new WaterParticle());
+				}
+			}
+		}elseif($this->ticksCaughtDelay > 0){
+			$this->ticksCaughtDelay -= $l;
+			$f1 = 0.15;
+
+			if($this->ticksCaughtDelay < 20){
+				$f1 = ($f1 + (20 - $this->ticksCaughtDelay) * 0.05);
+			}elseif($this->ticksCaughtDelay < 40){
+				$f1 = ($f1 + (40 - $this->ticksCaughtDelay) * 0.02);
+			}elseif($this->ticksCaughtDelay < 60){
+				$f1 = ($f1 + (60 - $this->ticksCaughtDelay) * 0.01);
+			}
+
+			if($this->random->nextFloat() < $f1){
+				$f9 = mt_rand(0, 360) * 0.01745;
+				$f2 = mt_rand(25, 60);
+				$d12 = $this->getPosition()->getX() + (sin($f9) * $f2 * 0.1);
+				$d14 = floor($this->getPosition()->getY()) + 1.0;
+				$d6 = $this->getPosition()->getZ() + (cos($f9) * $f2 * 0.1);
+				$block = $this->getWorld()->getBlock(new Vector3($d12, $d14 - 1, $d6));
+
+				if($block instanceof Water){
+					$this->getWorld()->addParticle(new Vector3($d12, $d14, $d6), new SplashParticle());
+				}
+			}
+
+			if($this->ticksCaughtDelay <= 0){
+				$this->ticksCatchableDelay = mt_rand(20, 80);
+				$this->fishApproachAngle = mt_rand(0, 360);
+			}
+		}else{
+			$this->ticksCaughtDelay = mt_rand(200, 900);
+			//TODO: Fishing speed.
+		}
+		if($this->ticksCatchable > 0){
+			$this->motion->y -= ($this->random->nextFloat() * $this->random->nextFloat() * $this->random->nextFloat()) * 0.2;
+		}
 	}
 
 	public function onRetraction() : void{
