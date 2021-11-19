d04ce845caa6c9759f6adf28193012f7bec3736a
NgLamIsSimp <nglamvn2911@gmail.com>
2021-11-19 14:14:44 +0700

    Fix start.cmd not work for pmmp.phar

--- start.cmd
+++ start.cmd
@@ -12,15 +12,19 @@ if exist bin\php\php.exe (
 if exist src/PocketMine.php (
 	set POCKETMINE_FILE=src/PocketMine.php
 ) else (
-	echo PocketMine-MP.phar not found
-	echo Downloads can be found at https://github.com/pmmp/PocketMine-MP/releases
-	pause
-	exit 1
+	if exist PocketMine-MP.phar(
+	    set POCKETMINE_FILE=PocketMine-MP.phar
+	) else (
+	    echo PocketMine-MP.phar not found
+        echo Downloads can be found at https://github.com/pmmp/PocketMine-MP/releases
+        pause
+        exit 1
+	)
 )
 
-if exist bin_php8\mintty.exe (
+if exist bin\mintty.exe (
 	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "PocketMine-MP" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
 ) else (
 	REM pause on exitcode != 0 so the user can see what went wrong
-	%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %* || pause
+	%PHP_BINARY% %POCKETMINE_FILE% %* || pause
 )
