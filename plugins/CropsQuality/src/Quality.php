e4cd921c7da714980be0a1806edcfc6925af0c2d
Ngọc Lam <nglamvn2911@gmail.com>
2022-06-28 18:20:42 +0700

    CropQuality

---
+++ plugins/CropsQuality/src/Quality.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+
+namespace CropsQuality;
+
+use pocketmine\utils\EnumTrait;
+
+/**
+ * This doc-block is generated automatically, do not modify it manually.
+ * This must be regenerated whenever registry members are added, removed or changed.
+ * @see build/generate-registry-annotations.php
+ * @generate-registry-docblock
+ *
+ * @method static Quality NORMAL()
+ * @method static Quality SILVER()
+ * @method static Quality GOLD()
+ * @method static Quality DIAMOND()
+ */
+
+class Quality{
+	use EnumTrait {
+		EnumTrait::__construct as Enum___construct;
+	}
+
+	protected static function setup() : void{
+		self::registerAll(
+			new self("normal", QualityIds::NORMAL, "Normal", QualityIds::NORMAL_COLOR),
+			new self("silver", QualityIds::SILVER, "Silver", QualityIds::SILVER_COLOR),
+			new self("gold", QualityIds::GOLD, "Gold", QualityIds::GOLD_COLOR),
+			new self("diamond", QualityIds::DIAMOND, "Diamond", QualityIds::DIAMOND_COLOR)
+		);
+	}
+
+	private int $id;
+	private string $name;
+	private string $color;
+
+	public function __construct(string $enumName, int $id, string $name, string $color){
+		$this->Enum___construct($enumName);
+		$this->id = $id;
+		$this->name = $name;
+		$this->color = $color;
+	}
+
+	public function getId(): int{
+		return $this->id;
+	}
+
+	public function getName(): string{
+		return $this->name;
+	}
+
+	public function getColor(): string{
+		return $this->color;
+	}
+
+	public static function fromId(int $id): ?Quality{
+		foreach(self::getAll() as $quality){
+			if($quality->getId() === $id){
+				return $quality;
+			}
+		}
+		return null;
+	}
+}
---
+++ plugins/CropsQuality/src/QualityIds.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace CropsQuality;
+
+final class QualityIds{
+	public const NORMAL = 0;
+	public const SILVER = 1;
+	public const GOLD = 2;
+	public const DIAMOND = 3;
+
+	public const NORMAL_COLOR = "§f";
+	public const SILVER_COLOR = "§9";
+	public const GOLD_COLOR = "§a";
+	public const DIAMOND_COLOR = "§b";
+}
