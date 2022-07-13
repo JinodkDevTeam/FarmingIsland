ca0e46983423d409c35007650b9fc790a3991ddd
Ngọc Lam <nglamvn2911@gmail.com>
2022-07-13 13:55:35 +0700

    CustomItem: Smol Cleanup

--- plugins/FI-CustomItems/src/item/CustomItemFactory.php
+++ plugins/FI-CustomItems/src/item/CustomItemFactory.php
@@ -84,20 +84,16 @@ class CustomItemFactory{
 		$this->register(new EnchantedItem(new CustomItemIdentifier(CustomItemIds::ENCHANTED_SPRUCE_WOOD), "Enchanted Spruce Wood", Rarity::COMMON(), VanillaBlocks::SPRUCE_WOOD()->asItem()));
 		$this->register(new EnchantedItem(new CustomItemIdentifier(CustomItemIds::ENCHANTED_JUNGLE_WOOD), "Enchanted Jungle Wood", Rarity::COMMON(), VanillaBlocks::JUNGLE_WOOD()->asItem()));
 		$this->register(new EnchantedItem(new CustomItemIdentifier(CustomItemIds::ENCHANTED_ACIA_WOOD), "Enchanted Acia Wood", Rarity::COMMON(), VanillaBlocks::ACACIA_WOOD()->asItem()));
-		$this->register(new EnchantedItem(new CustomItemIdentifier(CustomItemIds::ENCHANTED_DARK_OAK_WOOD), "Enchanted Dark Oak Wood", Rarity::COMMON(), VanillaBlocks::DARK_OAK_WOOD()->asItem()));
-
-		$this->register(new RefinedItem(new CustomItemIdentifier(CustomItemIds::REFINED_IRON), "Refined Iron", Rarity::RARE(), VanillaItems::LIGHT_GRAY_DYE()));
+		$this->register(new EnchantedItem(new CustomItemIdentifier(CustomItemIds::ENCHANTED_DARK_OAK_WOOD), "Enchanted Dark Oak Wood", Rarity::COMMON(), VanillaBlocks::DARK_OAK_WOOD()->asItem()));		$this->register(new RefinedItem(new CustomItemIdentifier(CustomItemIds::REFINED_IRON), "Refined Iron", Rarity::RARE(), VanillaItems::LIGHT_GRAY_DYE()));
 		$this->register(new RefinedItem(new CustomItemIdentifier(CustomItemIds::REFINED_GOLD), "Refined Gold", Rarity::RARE(), VanillaItems::YELLOW_DYE()));
 		$this->register(new RefinedItem(new CustomItemIdentifier(CustomItemIds::REFINED_EMERALD), "Refined Emerald", Rarity::RARE(), VanillaItems::LIME_DYE()));
 		$this->register(new RefinedItem(new CustomItemIdentifier(CustomItemIds::REFINED_DIAMOND), "Refined Diamond", Rarity::RARE(), VanillaItems::LIGHT_BLUE_DYE()));
 		$this->register(new RefinedItem(new CustomItemIdentifier(CustomItemIds::REFINED_TITANIUM), "Refined Titanium", Rarity::LEGENDARY(), VanillaBLocks::DIORITE()->asItem()));
-
 		$this->register(new NoicePaper(new CustomItemIdentifier(CustomItemIds::NOICE_PAPER), "Noice Paper", Rarity::LEGENDARY()));
 		$this->register(new Crook(new CustomItemIdentifier(CustomItemIds::CROOK), "Crook", Rarity::RARE()));
 		$this->register(new TitaniumDrill(new CustomItemIdentifier(CustomItemIds::TITANIUM_DRILL), "Titanium Drill", Rarity::VERY_SPECIAL()));
 		$this->register(new EnchantedItem(new CustomItemIdentifier(CustomItemIds::DRILL_FUEL), "Drill Fuel", Rarity::LEGENDARY(), VanillaItems::NETHER_STAR()));
 		$this->register(new BackpackSlot(new CustomItemIdentifier(CustomItemIds::BACKPACK_SLOT), "Backpack Slot", Rarity::LEGENDARY()));
-
 		$this->register(new EmeraldBlade(new CustomItemIdentifier(CustomItemIds::EMERALD_BLADE), "Emerald Blade", Rarity::EPIC()));
 		$this->register(new RefinedEmeraldBlade(new CustomItemIdentifier(CustomItemIds::REFINED_EMERALD_BLADE), "Refined Emerald Blade", Rarity::LEGENDARY()));
 		$this->register(new AspectOfTheEnd(new CustomItemIdentifier(CustomItemIds::ASPECT_OF_THE_END), "Aspect of the End", Rarity::RARE()));
@@ -107,7 +103,6 @@ class CustomItemFactory{
 		$this->register(new TreeCutter(new CustomItemIdentifier(CustomItemIds::TREECUTTER), "Tree Cutter", Rarity::LEGENDARY()));
 		$this->register(new BoosterBoots(new CustomItemIdentifier(CustomItemIds::BOOSTER_BOOTS), "Booster Boots", Rarity::LEGENDARY()));
 		$this->register(new SlimeBoots(new CustomItemIdentifier(CustomItemIds::SLIME_BOOTS), "Slime Boots", Rarity::LEGENDARY()));
-
 		$this->register(new OpRod(new CustomItemIdentifier(CustomItemIds::OP_ROD), "OP Rod", Rarity::SPECIAL()));
 		$this->register(new StarterRod(new CustomItemIdentifier(CustomItemIds::STARTER_ROD), "Starter Rod", Rarity::COMMON()));
 		$this->register(new OpAutomationRod(new CustomItemIdentifier(CustomItemIds::OP_AUTOMATION_ROD), "OP Automation Rod", Rarity::SPECIAL()));
@@ -119,7 +114,6 @@ class CustomItemFactory{
 		$this->register(new MechanicalRod(new CustomItemIdentifier(CustomItemIds::MECHANICAL_ROD), "Mechanical Rod", Rarity::RARE()));
 		$this->register(new AncientRod(new CustomItemIdentifier(CustomItemIds::ANCIENT_ROD), "Ancient Rod", Rarity::EPIC()));
 		$this->register(new GrapplingHook(new CustomItemIdentifier(CustomItemIds::GRAPPLING_HOOK), "Grappling Hook", Rarity::UNCOMMON()));
-
 		$this->register(new GameBreaker(new CustomItemIdentifier(CustomItemIds::GAME_BREAKER), "Game Breaker", Rarity::SPECIAL()));
 		$this->register(new GameAnnihilator(new CustomItemIdentifier(CustomItemIds::GAME_ANNIHILATOR), "Game Annihilator", Rarity::VERY_SPECIAL()));
 		$this->register(new AnnihilatorSword(new CustomItemIdentifier(CustomItemIds::ANNIHILATOR_SWORD), "Annihilator Sword", Rarity::ULTIMATE()));
