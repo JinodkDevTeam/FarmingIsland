dee84186cb8dd05df84d1581002b9fcd9caedc86
Ngọc Lam <nglamvn2911@gmail.com>
2022-08-07 05:43:45 +0700

    Ah

---
+++ plugins/DailyReward/src/DailyReward.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Nglam2911\DailyReward;
+
+use pocketmine\plugin\PluginBase;
+
+class DailyReward extends PluginBase{
+	protected function onEnable() : void{
+		//TODO: Implement onEnable()
+	}
+
+	protected function onDisable() : void{
+		//TODO: Implement onDisable()
+	}
+}
