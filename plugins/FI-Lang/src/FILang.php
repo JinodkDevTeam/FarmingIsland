b460ec032e6d63482d76c1bf24ba9e94dabb12b6
Ngọc Lam <nglamvn2911@gmail.com>
2022-08-30 11:08:33 +0700

    FI-Lang (WIP)

---
+++ plugins/FI-Lang/src/FILang.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace JinodkDevTeam\FILang;
+
+use pocketmine\plugin\PluginBase;
+
+class FILang extends PluginBase{
+	//NOTHING YET
+}
---
+++ plugins/FI-Lang/src/TranslationKeys.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace JinodkDevTeam\FILang;
+
+final class TranslationKeys{
+
+	#GLOBAL
+	public const COMMAND_INGAME = "command.ingame";
+
+	#Backpack
+	public const BACKPACK_NOTHAVE = "backpack.nothave";
+	public const BACKPACK_OPENFAIL = "backpack.openfail";
+	public const BACKPACK_OPENFAIL2 = "backback.openfail2";
+	public const BACKPACK_UI_TITLE = "backpack.ui.title";
+	public const BACKPACK_UI_BUTTON = "backpack.ui.button";
+	public const BACKPACK_GUI_NAME = "backpack.gui.name";
+
+	#Bank
+
+}
