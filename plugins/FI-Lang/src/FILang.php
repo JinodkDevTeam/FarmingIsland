a717d4e63cc02a5869f58f642d684555d25cc0ad
Ngọc Lam <nglamvn2911@gmail.com>
2022-10-13 07:23:08 +0700

    Prepare for transtions keys...

---
+++ plugins/FI-Lang/src/FILang.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace FILang;
+
+use pocketmine\plugin\PluginBase;
+
+class FILang extends PluginBase{
+	protected function onLoad() : void{
+		//TODO: Load Language file
+	}
+}
