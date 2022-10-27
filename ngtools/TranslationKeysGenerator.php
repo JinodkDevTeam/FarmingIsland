d39436780f301b60369cac120eca812ad9d6752d
Ngọc Lam <nglamvn2911@gmail.com>
2022-10-27 08:53:46 +0700

    WIP Translation Factory generator.

--- ngtools/TranslationKeysGenerator.php
+++ ngtools/TranslationKeysGenerator.php
@@ -39,6 +39,34 @@ HEADER;
 	echo "Done generating TranslationFactory.\n";
 }
 
+function generate_translation_factory() : void{
+	$data = parse_ini_file(LANG_DIRECTORY, false, INI_SCANNER_RAW);
+	if(!$data){
+		echo "Cannot open file " . LANG_DIRECTORY;
+		return;
+	}
+	ob_start();
+	$keys = array_keys($data);
+	echo <<<'HEADER'
+<?php
+declare(strict_types=1);
 
+namespace FILang;
+
+use pocketmine\lang\Translatable;
+
+class TranslationFactory{
+	/**
+	* This class is generated automatically, do NOT modify it by hand.
+	* See ngtools\TranslationKeysGenerator.php
+ 	*/
+HEADER;
+
+	foreach($keys as $key){
+		$string = str_replace(".", "_", $key);
+		$string = strtolower($string);
+
+	}
+}
 
 generate_translation_keys();
