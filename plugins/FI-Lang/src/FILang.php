cfebcb0aa93d7203c21303273d254c024011ed91
Ngọc Lam <nglamvn2911@gmail.com>
2022-10-27 10:09:26 +0700

    WIP Language loader.

--- plugins/FI-Lang/src/FILang.php
+++ plugins/FI-Lang/src/FILang.php
@@ -3,10 +3,26 @@ declare(strict_types=1);
 
 namespace FILang;
 
+use pocketmine\lang\Language;
+use pocketmine\lang\Translatable;
 use pocketmine\plugin\PluginBase;
 
 class FILang extends PluginBase{
+
+	public const SUPPORTED_LANGUAGES = [
+		"eng"
+	];
+	/** @var Language[] */
+	public static array $languages = [];
+
 	protected function onLoad() : void{
-		//TODO: Load Language file
+		foreach(self::SUPPORTED_LANGUAGES as $lang){
+			$this->saveResource("$lang.ini");
+			self::$languages[$lang] = new Language($lang, $this->getDataFolder() . "$lang.ini");
+		}
+	}
+
+	public static function translate(string $lang, Translatable $translatable) : string{
+		return self::$languages[$lang]->translate($translatable);
 	}
 }
