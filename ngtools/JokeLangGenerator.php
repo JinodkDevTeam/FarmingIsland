b17889862de9d3dd182cbf5441af46cfed938b48
Ngọc Lam <nglamvn2911@gmail.com>
2022-11-01 15:21:38 +0700

    Bruh

--- ngtools/JokeLangGenerator.php
+++ ngtools/JokeLangGenerator.php
@@ -42,20 +42,21 @@ function generateLang(Closure $formatCallback, string $target) : void{
 	file_put_contents($target, $contents);
 }
 
-function RandomCapsProtectParams(string $value) : string{
-	$parameterRegex = '/{%(.+?)}/';
-	$match = preg_match_all($parameterRegex, $value, $matches);
-	if($match > 0){
-		$str = implode("", array_map(fn($char) => (rand(0, 1) === 1) ? strtoupper($char) : $char, str_split(strtolower(str_replace($matches[0], "_", $value, $count)))));
-		for($i = 0; $i < $count; $i++){
-			$str = substr_replace($str, $matches[0][$i], strpos($str, "_"), 1);
-		}
-		return $str;
+function AddProtectParams(string $value, array $params, int $paramCount, string $find = "_") : string{
+	for($i = 0; $i < $paramCount; $i++){
+		$value = substr_replace($value, $params[0][$i], strpos($value, $find), strlen($find));
 	}
-	return implode("", array_map(fn($char) => (rand(0, 1) === 1) ? strtoupper($char) : $char, str_split(strtolower($value))));
+	return $value;
 }
 
-function RemoveProtectParams(string $value, array &$matches, int &$count, string $replace_value = "_") : string{
+function AddProtectColorFormat(string $value, array $colors, int $colorCount, string $find = "^") : string{
+	for($i = 0; $i < $colorCount; $i++){
+		$value = substr_replace($value, $colors[0][$i], strpos($value, $find), strlen($find));
+	}
+	return $value;
+}
+
+function RemoveProtectParams(string $value, &$matches, &$count, string $replace_value = "_") : string{
 	$parameterRegex = '/{%(.+?)}/';
 	$match = preg_match_all($parameterRegex, $value, $matches);
 	if ($match > 0) {
@@ -65,7 +66,7 @@ function RemoveProtectParams(string $value, array &$matches, int &$count, string
 }
 
 function RemoveProtectColorFormat(string $value, &$matches, &$count, string $replace_value = "^") : string{
-	$parameterRegex = '/§([0-9a-f])/';
+	$parameterRegex = '/§([0-9a-gk-or])/';
 	$match = preg_match_all($parameterRegex, $value, $matches);
 	if ($match > 0) {
 		return str_replace($matches[0], $replace_value, $value, $count);
@@ -73,5 +74,17 @@ function RemoveProtectColorFormat(string $value, &$matches, &$count, string $rep
 	return $value;
 }
 
+function RandomCaps(string $value) : string{
+	$str = RemoveProtectParams($value, $params, $paramCount, "_");
+	$str = RemoveProtectColorFormat($str, $colors, $colorCount, "^");
+	$str = implode("", array_map(fn($char) => (rand(0, 1) === 1) ? strtoupper($char) : $char, str_split(strtolower($str))));
+	$colors = $colors??[];
+	$params = $params??[];
+	$colorCount = $colorCount??0;
+	$paramCount = $paramCount??0;
+	$str = AddProtectColorFormat($str, $colors, $colorCount, "^");
+	return AddProtectParams($str, $params, $paramCount, "_");
+}
+
 generateLang(fn($value) => "Router", "plugins/FI-Lang/resources/lang/router.ini");
-generateLang(fn($value) => RandomCapsProtectParams($value), "plugins/FI-Lang/resources/lang/randomCaps.ini");
+generateLang(fn($value) => RandomCaps($value), "plugins/FI-Lang/resources/lang/randomCaps.ini");
