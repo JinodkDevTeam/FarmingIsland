430540d53fd9be7f811b0f58565c0f238f08bc94
Ngọc Lam <nglamvn2911@gmail.com>
2023-04-19 20:21:49 +0700

    FI-Utils: Rework item saver depend on nbt serializer.

--- virions/FI-Utils/src/JinodkDevTeam/utils/ItemUtils.php
+++ virions/FI-Utils/src/JinodkDevTeam/utils/ItemUtils.php
@@ -4,39 +4,51 @@ declare(strict_types=1);
 namespace JinodkDevTeam\utils;
 
 use CustomAddons\item\CustomItems;
-use Exception;
 use pocketmine\inventory\Inventory;
 use pocketmine\item\Item;
 use pocketmine\item\StringToItemParser;
+use pocketmine\nbt\LittleEndianNbtSerializer;
+use pocketmine\nbt\NBT;
+use pocketmine\nbt\NbtDataException;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\nbt\TreeRoot;
 use pocketmine\utils\TextFormat;
 use RuntimeException;
 
 class ItemUtils{
+
+	private static function isCIEnabled(): bool{
+		//Make FI-CustomAddon optional
+		return class_exists(CustomItems::class);
+	}
 
 	public static function toName(string $id): string{
 		$item = StringToItemParser::getInstance()->parse($id);
 		if (!is_null($item)){
 			return $item->getName();
 		}
-		$citem = CustomItems::get($id);
-		if ($citem == null){
-			return "";
+		if (self::isCIEnabled()){
+			$citem = CustomItems::get($id);
+			return is_null($citem)?"":$citem->getName();
 		}
-		return $citem->getName();
+		return "";
 	}
 
 	public static function toId(Item $item): string{
-		$nbt = $item->getNamedTag();
-		if ($nbt->getTag("CustomItemID") == null){
-			return self::vanillaItemMapping($item);
+		if (self::isCIEnabled()){
+			$nbt = $item->getNamedTag();
+			if ($nbt->getTag("CustomItemID") !== null){
+				$namespaceid = (string)$nbt->getTag("CustomItemID")->getValue();
+				//Verify
+				$citem = CustomItems::get($namespaceid);
+				if (is_null($citem)){
+					throw new RuntimeException(TextFormat::RED . "Unknow Custom Item namespace ID ! " . $namespaceid);
+				}
+				return $namespaceid;
+			}
 		}
-		$namespaceid = (string)$nbt->getTag("CustomItemID")->getValue();
-		//Verify
-		$citem = CustomItems::get($namespaceid);
-		if (is_null($citem)){
-			throw new RuntimeException(TextFormat::RED . "Unknow Custom Item namespace ID ! " . $namespaceid);
-		}
-		return $namespaceid;
+		return self::vanillaItemMapping($item);
 	}
 
 	/**
@@ -67,8 +79,13 @@ class ItemUtils{
 		if (!is_null($item)){
 			return $item;
 		}
-		$citem = CustomItems::get($id);
-		return $citem?->toItem();
+		if (self::isCIEnabled()){
+			$citem = CustomItems::get($id);
+			if (!is_null($citem)){
+				return $citem->toItem();
+			}
+		}
+		return null;
 	}
 
 	/**
@@ -129,43 +146,21 @@ class ItemUtils{
 		return $itemSlots;
 	}
 
-	public static function toString(Item $item): string{
+	public static function toBinaryString(Item $item): string{
 		$nbt = $item->nbtSerialize();
-		return mb_convert_encoding(serialize($nbt), 'UTF-8');
+		$writter = new LittleEndianNbtSerializer();
+		return $writter->write(new TreeRoot($nbt));
 	}
 
-	public static function fromString(string $string): ?Item{
+	public static function fromBinaryString(string $string): ?Item{
+		$reader = new LittleEndianNbtSerializer();
+		$root = $reader->read($string);
 		try{
-			return Item::nbtDeserialize(unserialize(mb_convert_encoding($string, 'ISO-8859-1')));
-		}catch(Exception){
+			$nbt = $root->mustGetCompoundTag();
+		}catch(NbtDataException $exception){
 			return null;
 		}
-	}
-
-	/**
-	 * @param Item[] $items
-	 *
-	 * @return string[]
-	 */
-	public static function ItemArraytoStringArray(array $items): array{
-		$data = [];
-		foreach($items as $item){
-			$data[] = self::toString($item);
-		}
-		return $data;
-	}
-
-	/**
-	 * @param string[] $data
-	 *
-	 * @return Item[]
-	 */
-	public static function StringArrayToItemArray(array $data): array{
-		$items = [];
-		foreach($data as $d){
-			$items[] = self::fromString($d);
-		}
-		return $items;
+		return Item::nbtDeserialize($nbt);
 	}
 
 	/**
@@ -173,12 +168,14 @@ class ItemUtils{
 	 *
 	 * @return string
 	 */
-	public static function ItemArray2string(array $items): string{
-		$data = [];
+	public static function itemArray2binString(array $items): string{
+		$contents = [];
 		foreach($items as $item){
-			$data[] = $item->nbtSerialize();
+			$contents[] = $item->nbtSerialize();
 		}
-		return mb_convert_encoding(serialize($data), 'UTF-8');
+		$listTag = new ListTag($contents, NBT::TAG_Compound);
+		$writter = new LittleEndianNbtSerializer();
+		return $writter->write(new TreeRoot($listTag));
 	}
 
 	/**
@@ -186,13 +183,22 @@ class ItemUtils{
 	 *
 	 * @return Item[]
 	 */
-	public static function string2ItemArray(string $data): array{
-		if ($data == "") return [];
-		$array = unserialize(mb_convert_encoding($data, 'ISO-8859-1'));
-		$items = [];
-		foreach($array as $nbt){
-			$items[] = Item::nbtDeserialize($nbt);
+	public static function binString2itemArray(string $data): array{
+		$contents = [];
+		$reader = new LittleEndianNbtSerializer();
+		$root = $reader->read($data);
+		/** @var ListTag $listTag */
+		$listTag = $root->getTag();
+		if (!$listTag instanceof ListTag){
+			return [];
 		}
-		return $items;
+		if ($listTag->getTagType() !== NBT::TAG_Compound){
+			return [];
+		}
+		/** @var CompoundTag $tag */
+		foreach($listTag as $tag){
+			$contents[] = Item::nbtDeserialize($tag);
+		}
+		return $contents;
 	}
 }
